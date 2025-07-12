4805a68f70f74b8478293217b422ac053ba64522
ahmad4you <ahmad.alrefai@outlook.de>
2025-07-13 00:11:46 +0200

    refactor

--- Scooping/src/main/java/home/ahmad/obst/ObstBean.java
+++ Scooping/src/main/java/home/ahmad/obst/ObstBean.java
@@ -13,32 +13,37 @@ import java.util.List;
  */
 @Named
 @SessionScoped
-public class ObstBean implements Serializable{
+public class ObstBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	 private String feld1;
-	 private String feld2;
+	private String feld1;
+	private String feld2;
 
 	public List<Obst> getObstListe() {
-        return Arrays.asList(
-            new Obst("Apfel", "bi bi-apple"),
-            new Obst("Banane", "bi bi-emoji-smile"),
-            new Obst("Kirsche", "bi bi-heart-fill"),
-            new Obst("Traube", "bi bi-droplet"),
-            new Obst("Orange", "bi bi-sun-fill")
-        );
-    }
-	
-	 public void kopiere(Obst obst) {
-	        this.feld1 = obst.getName();       
-	        this.feld2 = obst.getBootstrapIcon(); 
-	    }
+		return Arrays.asList(new Obst("Apfel", "bi bi-apple"), new Obst("Banane", "bi bi-emoji-smile"),
+				new Obst("Kirsche", "bi bi-heart-fill"), new Obst("Traube", "bi bi-droplet"),
+				new Obst("Orange", "bi bi-sun-fill"));
+	}
 
-	    public String getFeld1() { return feld1; }
-	    public void setFeld1(String feld1) { this.feld1 = feld1; }
+	public void kopiere(Obst obst) {
+		this.feld1 = obst.getName();
+		this.feld2 = obst.getBootstrapIcon();
+	}
 
-	    public String getFeld2() { return feld2; }
-	    public void setFeld2(String feld2) { this.feld2 = feld2; }
-   
+	public String getFeld1() {
+		return feld1;
+	}
+
+	public void setFeld1(String feld1) {
+		this.feld1 = feld1;
+	}
+
+	public String getFeld2() {
+		return feld2;
+	}
+
+	public void setFeld2(String feld2) {
+		this.feld2 = feld2;
+	}
+
 }
-
