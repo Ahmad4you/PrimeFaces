17f06c44247c198db91d0019575311c9ca4426bc
ahmad4you <ahmad.alrefai@outlook.de>
2025-07-13 18:04:54 +0200

    refactoring

--- Scooping/src/main/java/home/ahmad/resources/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package home.ahmad.resources;
-
-import jakarta.ws.rs.core.Application;
-import jakarta.ws.rs.ApplicationPath;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-    
-}
